dd8a113c1b6dc9130d106dd99e810e4075e7dcf9
dat <redknight785@gmail.com>
2025-07-31 12:57:46 +0700

    fix typo 2

--- src/routes/api/food.js
+++ src/routes/api/food.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Food = require('../models/Food');
+const Food = require('../../models/Food');
 
 // GET /api/food - get all food items
 router.get('/', async (req, res) => {
