a3140268852ef651f05f2c53ec56834421f691c9
TienDat8605 <98029718+TienDat8605@users.noreply.github.com>
2025-09-03 11:02:35 +0700

    Update food.js

--- src/routes/api/food.js
+++ src/routes/api/food.js
@@ -6,7 +6,6 @@ const foodController = require('../../controllers/foodController');
 // GET /api/food - get all food items
 // not passing authMiddleware here for development purposes, but should be protected in production
 router.get('/', foodController.handleGetAllFoods);
-router.get('/:id/image', foodController.handleGetFoodImageByID);
 router.post('/searchByEmbedding', foodController.handleSearchByEmbedding);
 // Search food items by category
 router.post('/searchByCategory', foodController.handleSearchByCategory);
@@ -14,8 +13,9 @@ router.post('/searchByCategory', foodController.handleSearchByCategory);
 router.post('/searchBySubcategory', foodController.handleSearchBySubcategory);
 // search food items by category, subcategory, and rating
 router.post('/search', foodController.handleSearch);
-// get food item by ID
-router.get('/:id', foodController.handleGetFoodByID);
 // get best sellers food items
 router.get('/bestSellers', foodController.handleGetBestSellers);
+// get food item by ID
+router.get('/:id', foodController.handleGetFoodByID);
+router.get('/:id/image', foodController.handleGetFoodImageByID);
 module.exports = router;
